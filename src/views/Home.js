5e3be4c9593ef2e6887695e3f70ce75530be71f4
Aman <Aman-at-280608293403>
2023-02-21 19:39:06 +0500

    updated

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import imgOne from "../../assets/1.png";
 
 const Home = () => {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Button title="Drawer" />
-        <Text>All Books</Text>
-        <Button title="Search" />
+        <View style={styles.imgsContainer}>
+          <View>
+            <Image source={imgOne} style={styles.image} />
+          </View>
+          <View>
+            <Image source={imgOne} style={styles.image} />
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
@@ -20,6 +33,16 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     marginHorizontal: 15,
+  },
+  image: {
+    width: "50%",
+    height: 100,
+  },
+  imgsContainer: {
+    // display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    flex: 1,
   },
 });
 
