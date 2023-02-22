1ca025080b8d90bd865361781db661cb51f8553e
Aman <Aman-at-280608293403>
2023-02-22 18:59:24 +0500

    updated

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Button,
+  Dimensions,
   Image,
   ScrollView,
   StyleSheet,
@@ -8,16 +9,78 @@ import {
   View,
 } from "react-native";
 import imgOne from "../../assets/1.png";
+import imgTwo from "../../assets/2.jpg";
 
+const screen = Dimensions.get("screen");
 const Home = () => {
   return (
     <ScrollView>
       <View style={styles.container}>
         <View style={styles.imgsContainer}>
-          <View>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+              marginRight: 10,
+            }}
+          >
+            <Image source={imgTwo} style={{ ...styles.image }} />
+          </View>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+            }}
+          >
             <Image source={imgOne} style={styles.image} />
           </View>
-          <View>
+        </View>
+        <View style={styles.imgsContainer}>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+              marginRight: 10,
+            }}
+          >
+            <Image source={imgOne} style={{ ...styles.image }} />
+          </View>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+            }}
+          >
+            <Image source={imgOne} style={styles.image} />
+          </View>
+        </View>
+        <View style={styles.imgsContainer}>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+              marginRight: 10,
+            }}
+          >
+            <Image source={imgOne} style={{ ...styles.image }} />
+          </View>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+            }}
+          >
+            <Image source={imgOne} style={styles.image} />
+          </View>
+        </View>
+        <View style={styles.imgsContainer}>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+              marginRight: 10,
+            }}
+          >
+            <Image source={imgOne} style={{ ...styles.image }} />
+          </View>
+          <View
+            style={{
+              ...styles.imgSingleContainer,
+            }}
+          >
             <Image source={imgOne} style={styles.image} />
           </View>
         </View>
@@ -28,21 +91,26 @@ const Home = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    justifyContent: "space-between",
-    flexDirection: "row",
-    alignItems: "center",
     marginHorizontal: 15,
+    marginTop: 20,
   },
   image: {
-    width: "50%",
-    height: 100,
+    // width: 50,
+    height: 200,
+    width: (screen.width - 40) * 0.5,
+    resizeMode: "cover",
+    borderRadius: 10,
   },
   imgsContainer: {
-    // display: "flex",
-    justifyContent: "space-between",
-    flexDirection: "row",
     flex: 1,
+    flexDirection: "row",
+    columnGap: 10,
+    flexWrap: "wrap",
+    marginBottom: 10,
+  },
+  imgSingleContainer: {
+    height: 200,
+    width: (screen.width - 40) * 0.5,
   },
 });
 
