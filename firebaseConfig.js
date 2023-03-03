0040ad8b6e3111c11d88107f2433036bf3c5e05c
Aman <Aman-at-280608293403>
2023-03-03 19:28:51 +0500

    firebase config added

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFn-WeiSb9XbQHHJh3GoH2VA-X8YrHgcU",
+  authDomain: "books-app-cd393.firebaseapp.com",
+  projectId: "books-app-cd393",
+  storageBucket: "books-app-cd393.appspot.com",
+  messagingSenderId: "162433182162",
+  appId: "1:162433182162:web:139d401e4304de313c400b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
